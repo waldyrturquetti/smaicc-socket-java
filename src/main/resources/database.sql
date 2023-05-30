115499d547bf22aacbf2a058b94ec4a13042a947
waldyrturquetti <waldyrturquetti@bemol.com.br>
2023-05-30 19:42:27 -0300

    feat: add more things in README.md .

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -20,9 +20,9 @@ create table incident
     date         date         not null,
     hour         time         not null,
     state        char(2)      not null,
-    city         varchar(255) not null,
-    neighborhood varchar(255) not null,
-    street       varchar(255) not null,
+    city         varchar(50) not null,
+    neighborhood varchar(50) not null,
+    street       varchar(50) not null,
     incident_type enum(
         'ALAGAMENTO', 'DESLIZAMENTO', 'ACIDENTE_DE_CARRO', 'OBSTRUCAO_DA_VIA', 'FISSURA_DA_VIA',
         'PISTA_EM_OBRAS','LENTIDAO_NA_PISTA', 'ANIMAIS_NA_PISTA', 'NEVOEIRO', 'TROMBA_DAGUA'),
